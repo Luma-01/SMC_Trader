d8a276f9c4f035f02971c940a8100dc1ee1c3e49
Luma-01 <mh-god@naver.com>
2025-05-01 17:01:56 +0900

    Create fvg.py

---
+++ core/fvg.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from typing import List, Dict
+
+def detect_fvg(df: pd.DataFrame) -> List[Dict]:
+    fvg_zones = []
+
+    for i in range(2, len(df)):
+        c1 = df.iloc[i - 2]
+        c3 = df.iloc[i]
+
+        # 상승 FVG
+        if c1['high'] < c3['low']:
+            fvg_zones.append({
+                "type": "bullish",
+                "low": c3['low'],
+                "high": c1['high'],
+                "time": df['time'].iloc[i]
+            })
+
+        # 하락 FVG
+        elif c1['low'] > c3['high']:
+            fvg_zones.append({
+                "type": "bearish",
+                "low": c1['low'],
+                "high": c3['high'],
+                "time": df['time'].iloc[i]
+            })
+
+    return fvg_zones
