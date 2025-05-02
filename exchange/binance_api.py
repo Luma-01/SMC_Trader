ba0cd7b4e7563726f8457aaa0c51a392ae9840e7
Luma-01 <mh-god@naver.com>
2025-05-02 21:54:03 +0900

    binance_api.py 업데이트

--- exchange/binance_api.py
+++ exchange/binance_api.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 api_key = os.getenv("BINANCE_API_KEY")
 api_secret = os.getenv("BINANCE_API_SECRET")
-client = Client(api_key, api_secret)
+client = Client(api_key, api_secret, tld='com')
 
 print("[BINANCE API] Loaded key:", os.getenv("BINANCE_API_KEY"))
 
